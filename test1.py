ad41d0646b62a01574f57b822b17aa0e9e19f6a7
Imesh <ikaushalya7172800@gmail.com>
2025-05-04 17:25:51 +0530

    get user inputs

--- test1.py
+++ test1.py
@@ -1,2 +1,5 @@
-i= int(input("Enter : "))  #integer
-y= float (input("Enter : "))
+ 
+name= input("Enter your name: ")  #get user input
+age= int(input("Enter your age: "))
+print(f"Hello,{name}im!, You are {age} , year old.")
+ 
