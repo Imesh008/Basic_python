9909e65c43ce003d9b6a4cabb778a04b60f99156
Imesh <ikaushalya7172800@gmail.com>
2025-05-05 21:29:26 +0530

    greeting message

--- test1.py
+++ test1.py
@@ -1,4 +1,7 @@
  
 name= input("Enter your name: ")  #get user input
 age= int(input("Enter your age: "))
-print(f"Hello,{name}im!, You are {age} , year old.")
+print(f"Hello,{name}!, You are {age} , years old.")
+#example using variables
+birth_year =2025- age #estimate birth year
+print("You were probably born in", birth_year)
