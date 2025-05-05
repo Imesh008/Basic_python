61dab959b0f6a9f9f604a9df4b83852579772206
Imesh <ikaushalya7172800@gmail.com>
2025-05-05 22:16:11 +0530

    decorate

--- test1.py
+++ test1.py
@@ -2,7 +2,7 @@
 name= input("Enter your name: ")  #get user input
 age= int(input("Enter your age: "))
 print(f"Hello,{name}!, You are {age} , years old.")
-
+print("-" * 20)
 #example using variables
 current_year = 2025
 birth_year =current_year- age #estimate birth year
