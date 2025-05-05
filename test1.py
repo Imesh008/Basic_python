bd49dc00d41b9591007490fbb4a0bc2f2161a473
Imesh <ikaushalya7172800@gmail.com>
2025-05-05 22:07:27 +0530

    change variable

--- test1.py
+++ test1.py
@@ -2,6 +2,8 @@
 name= input("Enter your name: ")  #get user input
 age= int(input("Enter your age: "))
 print(f"Hello,{name}!, You are {age} , years old.")
+
 #example using variables
-birth_year =2025- age #estimate birth year
+current_year = 2025
+birth_year =current_year- age #estimate birth year
 print("You were probably born in", birth_year)
