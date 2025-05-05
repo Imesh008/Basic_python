c1c8569b61fba8b56b95781d5684b2cd1e0d3037
Imesh <ikaushalya7172800@gmail.com>
2025-05-05 22:24:52 +0530

    rearrange

--- test1.py
+++ test1.py
@@ -1,8 +1,9 @@
- 
+#get user input
 name= input("Enter your name: ")  #get user input
 age= int(input("Enter your age: "))
 print(f"Hello,{name}!, You are {age} , years old.")
 print("-" * 20)
+
 #example using variables
 current_year = 2025
 birth_year =current_year- age #estimate birth year
