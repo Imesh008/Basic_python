a8abd816372d54ae4963457315fd36edf0a2250d
Imesh <ikaushalya7172800@gmail.com>
2025-05-07 20:41:02 +0530

    for loop

--- func.py
+++ func.py
@@ -8,4 +8,11 @@ print(greet("Ayesh"))
 print("-"*25)
 
 #create a list
-fruits=["apple","banana","cherry"]
+fruits=["apple","banana","cherry"]  #list
+
+#append item to the list
+fruits.append ("mango")
+
+#for loop to print each fruit
+for fruit in fruits:
+    print(fruit)
