48e772c840a8f8a3d4add8216601d96f1a2ea1f5
Imesh <ikaushalya7172800@gmail.com>
2025-05-09 12:35:57 +0530

    print confirmation

--- file_handling.py
+++ file_handling.py
@@ -1,3 +1,6 @@
 #create and opne a file for writing
 with open("hello.txt" , "w") as file:
     file.write("Hello, file !")
+
+#add print to confirm file write
+print("File Written Successfully...")
