a851cadd0e91dffa428935453ab9a309e6193331
Imesh <ikaushalya7172800@gmail.com>
2025-05-10 21:18:48 +0530

    view all stored tasks

--- mini_project.py
+++ mini_project.py
@@ -2,10 +2,20 @@
 tasks = []   #initialize the tasks list
 
 #define a function
-def add_tasl(task):
+def add_task(task):
     tasks.append(task)
 
 #define show_tasks function
 def show_tasks():
     for i , task in enumerate (tasks , 1) :
         print (f"(i). {task}")
+
+#add main loo structure
+while True:
+    print("\n1. Add Task\n2. View Tasks\n3. Exit")
+    choice =input("choose: ")
+
+#handle task addition option
+if choice == "1":
+    task = input ("Enter task: ")
+    add_task(task)
