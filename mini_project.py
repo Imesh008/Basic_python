6eb18ea80911d08b93fa2edcd9b5394f14917671
Imesh <ikaushalya7172800@gmail.com>
2025-05-10 21:47:51 +0530

    end

--- mini_project.py
+++ mini_project.py
@@ -7,20 +7,26 @@ def add_task(task):
 
 #define show_tasks function
 def show_tasks():
-    for i , task in enumerate (tasks , 1) :
-        print (f"(i). {task}")
+    for i , task in enumerate(tasks , 1) :
+        print (f"{i}. {task}")
 
-#add main loo structure
+#add main loop structure
 while True:
     print("\n1. Add Task\n2. View Tasks\n3. Exit")
     choice =input("choose: ")
 
 #handle task addition option
-if choice == "1":
-    task = input ("Enter task: ")
-    add_task(task)
+    if choice == "1":
+        task = input ("Enter task: ")
+        add_task(task)
 
-#handle task addition option
-elif choice == "2":
-    show_tasks()
-    
+#handle view task option
+    elif choice == "2":
+        show_tasks()
+
+#exit and default case 
+    elif choice == "3":
+        print("Bye...!")
+        break
+    else:
+        print ("Invalid choice...")    
