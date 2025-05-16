2b63785cf1ba3d8bc3fd15daf87fc7f0d708687a
Imesh <ikaushalya7172800@gmail.com>
2025-05-16 13:22:36 +0530

    end

--- proj2_calculator.py
+++ proj2_calculator.py
@@ -69,9 +69,9 @@ elif choice == "3":
     print ("Result: ", multiply (num1, num2))
     input ("Press Enter to continue")
 
-
 elif choice == "4":
     print ("Result: ",divide(num1, num2))
+    input ("Press Enter to continue")
 
 else:
     print("Invalid option... Try again...")
