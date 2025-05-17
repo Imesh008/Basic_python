b7f2858452d5c3febf8c708e82083a4eda28265a
Imesh <ikaushalya7172800@gmail.com>
2025-05-17 09:37:51 +0530

    set exit massege

--- proj2_calculator.py
+++ proj2_calculator.py
@@ -44,6 +44,7 @@ while True :
 
     if choice == "5":   #choice number 5 
         print ("Existing calculator... Goodbye ...!")
+        print("Thanks for using the calculator...!")        #exit message
         break
 
 
