d150a3807599b2f6d8dac11533615ae53a43d85f
Imesh <ikaushalya7172800@gmail.com>
2025-05-29 17:48:21 -1100

    clean code

--- proj2_calculator.py
+++ proj2_calculator.py
@@ -93,7 +93,6 @@ while True :
         input ("Press Enter to continue...")
 
 
-
     elif choice == "2":
         result = subtract(num1, num2)
         history.append(f"{num1} - {num2} = {result}")
