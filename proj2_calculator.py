f57dd5f1f473d6dcd75df00be2e79b13192dcd59
Imesh <ikaushalya7172800@gmail.com>
2025-06-06 10:01:09 +0530

    choces check

--- proj2_calculator.py
+++ proj2_calculator.py
@@ -103,15 +103,15 @@ def run (self):
             input("Press Enter to continue...")
             continue
  
-#Input numbers
-#Add try-except for number input
-    elif choice in ["1", "2", "3", "4", "5", "6"]:
-        try:
-            num1 = float(input("Enter first number: "))
-            num2 = float(input("Enter second number: ")) #getting user inputs
-        except ValueError:
-            print("Invalid input. Please enter numbers only...!")
-            continue
+        #Input numbers
+        #Add try-except for number input
+        elif choice in ["1", "2", "3", "4", "5", "6"]:
+            try:
+                num1 = float(input("Enter first number: "))
+                num2 = float(input("Enter second number: ")) #getting user inputs
+            except ValueError:
+                print("Invalid input. Please enter numbers only...!")
+                continue
      
  
 
