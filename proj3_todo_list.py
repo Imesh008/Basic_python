2d1d711077960466fa3737a247b17a0e06edf883
Imesh <ikaushalya7172800@gmail.com>
2025-06-13 14:22:15 +0530

    read line and retrurn

--- proj3_todo_list.py
+++ proj3_todo_list.py
@@ -7,3 +7,10 @@ TASK_FILE = 'tasks.txt'
 #loads tasks from the file into a list
 def lead_tasks():
     #creat a file if it does not exist
+    if not os.path.ecists(TASK_FILE):
+        open(TASK_FILE, 'w').close()
+    
+    #read each line and return a list of tasks
+    with open (TASK_FILE, 'r') as file:
+        return [line.strip() for line in file.read()]
+    
