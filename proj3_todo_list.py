0fb6c7550ff5580593489b87917b646aef41768a
Imesh <ikaushalya7172800@gmail.com>
2025-06-20 14:31:38 +0530

    checking cinfrimation

--- proj3_todo_list.py
+++ proj3_todo_list.py
@@ -88,6 +88,7 @@ def main ():
 
         elif choice == "5":
             confirm = input("Do you want to  save tasks before exiting? (yes/no): ").strip().lower()
+            if confirm == "yes"or confirm == "y":
             print("Exiting the program. Goodbye!...")
             break
         else:
