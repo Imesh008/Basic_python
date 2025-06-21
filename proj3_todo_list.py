145153e1e03258d8c80da31aeec3613323d249fd
Imesh <ikaushalya7172800@gmail.com>
2025-06-21 19:44:58 +0530

    clean code

--- proj3_todo_list.py
+++ proj3_todo_list.py
@@ -28,6 +28,7 @@ def save_tasks(tasks):
 #Display menu options to the users 
 def display_menu():
     print ("\n" + "="*30)
+    print ("TODO LIST MENU")
     print ("\nTodo List Menu:")
     print ("1. View tasks")
     print ("2. Add tasks")
