c1902ba23022210d81df30b9544b08dbd1ba5838
Imesh <ikaushalya7172800@gmail.com>
2025-06-22 20:13:02 +0530

    clean code

--- proj3_todo_list.py
+++ proj3_todo_list.py
@@ -30,7 +30,6 @@ def display_menu():
     print ("\n" + "="*30)
     print ("TODO LIST MENU")
     print ( "="*30)
-     print ("\n")
     print ("1. View tasks")
     print ("2. Add tasks")
     print ("3. Remove tasks")
