f01d377c429fd3183def581e8249814216ed6c48
Imesh <ikaushalya7172800@gmail.com>
2025-06-28 19:49:43 +0530

    clean code

--- proj3_todo_list.py
+++ proj3_todo_list.py
@@ -31,7 +31,7 @@ class Todolist:
 
 #Display menu options to the users 
     def display_menu(self):
-    print ("\n" + "="*30)
+        print ("\n" + "="*30)
     print ("---- TODO LIST MENU ----")
     print ( "="*30)
     print ("1. Add tasks")
