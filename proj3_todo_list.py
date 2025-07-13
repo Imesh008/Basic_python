392b0aca80735392b7315fd3fd53751891a9d91f
Imesh <ikaushalya7172800@gmail.com>
2025-07-13 12:49:21 +0530

    clean code

--- proj3_todo_list.py
+++ proj3_todo_list.py
@@ -109,6 +109,7 @@ class Todolist:
                 clear_confirm = input("Are you sure you want to delete all tasks? (yes/no): ").strip().lower()
                 if clear_confirm == "yes" or clear_confirm == "y":
                     self.tasks.clear()
+                elif clear_confirm == "no" or clear_confirm == "n":
                     print("All tasks have been deleted successfully...!")
                 else:
                     print("No tasks were deleted...")
