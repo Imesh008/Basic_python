c530ef7c163375a9c0954f0af2dbc10d3b2acfd2
Imesh <ikaushalya7172800@gmail.com>
2025-07-17 04:48:59 +0530

    clean code

--- Expence_tracker.py
+++ Expence_tracker.py
@@ -15,7 +15,7 @@ if not os.path.exists(FILE_NAME):
 def add_expense():
     try:
         amount = float float(input("Enter amount(RS): "))
-        category = input("Enter category(Food, Travel, etc.): ")
+        category = input("Enter category(Food, Travel, etc.): ").strip()
         date = input("Enter date (YYYY-MM-DD) or leave a blank for today: ")
 
 
