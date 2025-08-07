62d959d8836595a95f4b4c0b5328036322556957
Imesh <ikaushalya7172800@gmail.com>
2025-08-07 15:36:23 +0530

    clean code

--- Expence_tracker.py
+++ Expence_tracker.py
@@ -116,8 +116,7 @@ def main_menu():
         print("5. Filter Expenses by date")
         print("6. Delete an Expense")
         print("7. Exit")
-        choice = input("Choose an option (1-7): ").strip()
-
+ 
 
   
         elif choice == '2':             ############################################
