4e3afa1c813631b38ae3cf88725772b06f6eefca
Imesh <ikaushalya7172800@gmail.com>
2025-08-08 15:39:29 +0530

    clean code

--- Expence_tracker.py
+++ Expence_tracker.py
@@ -107,8 +107,7 @@ def delete_expense():
  
 #add new options in the menue
 def main_menu():
-    while True:
-  #export filtered data to another file
+   #export filtered data to another file
 def export_filtered_data():
     category = input("Enter category to export: ").strip().lower()
     export_file = f"filtered_expenses_{category}.txt"
