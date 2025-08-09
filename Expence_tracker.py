7347f93cab9b1f982a4fe67caad1bebed65a97c9
Imesh <ikaushalya7172800@gmail.com>
2025-08-09 15:46:05 +0530

    clean code

--- Expence_tracker.py
+++ Expence_tracker.py
@@ -59,7 +59,6 @@ if name == "__main__":
     print("Welcome to the Expense Tracker! 📊")        
  
 
-
 #filter expenses by category
 def filter_expenses_by_category():
     keyword = input ("Enter category to filter: ").strip().lower()
