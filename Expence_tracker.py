c201cb38aec97c70c378589993e77d2defb0fba7
Imesh <ikaushalya7172800@gmail.com>
2025-08-20 09:02:31 +0530

    clean code

--- Expence_tracker.py
+++ Expence_tracker.py
@@ -91,9 +91,10 @@ def delete_expense():
         choice = int(input("Enter the number of the expense to delete: "))
         if 1 <= choice <= len(expenses):
             deleted = expenses.pop(choice - 1)
+            print(f"✅ Deleted expense: {deleted.strip()}\n")
         
 
-        
+
 #export filtered data 
 def export_filtered_data():
     category = input("Enter category to export: ").strip().lower()
