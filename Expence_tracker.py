e74cc60867d04e0738d3bca4ade2a5b75f5d7466
Imesh <ikaushalya7172800@gmail.com>
2025-08-23 20:24:31 +0530

    clean code

--- Expence_tracker.py
+++ Expence_tracker.py
@@ -116,6 +116,7 @@ def export_filtered_data():
             for line in f:
                 date, amount, cat, description = line.strip().split(',', 3)
                 if cat.lower() == category:
+                    f_out.write(line)
 
 
 
