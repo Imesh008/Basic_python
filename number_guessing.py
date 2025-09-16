32d3555f41aa21e53170e295b7bde22b0fdcc1a4
Imesh <ikaushalya7172800@gmail.com>
2025-09-16 15:40:22 +0530

    chceking difficulty

--- number_guessing.py
+++ number_guessing.py
@@ -3,8 +3,7 @@ import random
 number_to_guess =  random.randint (1, 100)
 attemps = 0
 diffculty = input ("Choose difficulty (Easy/Medium/Hard): ").lower()
-max_attemps = 10 
-if difficulty == "easy": 
+max_attemps = 10 if difficulty == "easy" else 7 if difficulty == "medium" else 5
  
 
 guess = int(input("Guess a number between 1 and 100: "))
