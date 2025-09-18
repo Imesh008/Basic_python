49dcabb084d668d4bee98a3feda4c2d5e5f0d12c
Imesh <ikaushalya7172800@gmail.com>
2025-09-18 16:08:36 +0530

    clean code

--- number_guessing.py
+++ number_guessing.py
@@ -1,7 +1,7 @@
 import random 
 
 number_to_guess =  random.randint (1, 100)
-attemps = 0
+attempts = 0
 difficulty = input ("Choose difficulty (Easy/Medium/Hard): ").lower()
 max_attempts = 10 if difficulty == "easy" else 7 if difficulty == "medium" else 5
  
