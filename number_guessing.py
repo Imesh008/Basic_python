a2fd88df594e31e4f58ad2a6f7bfd9cafdf70d32
Imesh <ikaushalya7172800@gmail.com>
2025-09-19 16:10:55 +0530

    clean code

--- number_guessing.py
+++ number_guessing.py
@@ -2,6 +2,7 @@ import random
 
 #Genarate a random number between 1 and 100
 number_to_guess =  random.randint (1, 100)
+
 attempts = 0
 difficulty = input ("Choose difficulty (Easy/Medium/Hard): ").lower()
 max_attempts = 10 if difficulty == "easy" else 7 if difficulty == "medium" else 5
