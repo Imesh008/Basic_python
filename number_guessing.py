703a4aebf8359173d2e4bbb5695d8f4a3c2577ab
Imesh <ikaushalya7172800@gmail.com>
2025-09-23 16:54:31 +0530

    clean code

--- number_guessing.py
+++ number_guessing.py
@@ -5,6 +5,9 @@ print("Welcome to the Number Guessing Game!")
 print("I'm thinking of a number between 1 and 100.")
 print("-"*30)
 
+
+
+
 #Generate a random number between 1 and 100
 number_to_guess = random.randint(1, 100)
 
