e737a9a219eb15421a343431ec0bdf26a72b1ed5
Imesh <ikaushalya7172800@gmail.com>
2025-09-24 19:47:10 +0530

    measuring the number size

--- number_guessing.py
+++ number_guessing.py
@@ -15,7 +15,7 @@ def play_game():
 
     while guess != number_to_guess:
         attempts += 1
-        
+        if guess < number_to_guess:
 
 #Generate a random number between 1 and 100
 number_to_guess = random.randint(1, 100)
