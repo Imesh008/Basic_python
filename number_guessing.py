eb21757c884e1708f43c568147761aeedfd1b1b4
Imesh <ikaushalya7172800@gmail.com>
2025-09-30 20:32:48 +0530

    clean code

--- number_guessing.py
+++ number_guessing.py
@@ -52,7 +52,7 @@ print("👋 Thanks for playing!")
  
  
  
-   = 10     == "easy" else 7 if difficulty == "medium" else 5
+             "easy" else 7 if difficulty == "medium" else 5
 #Ask user for the first guess
 
 guess = int(input("Guess a number between 1 and 100: "))
