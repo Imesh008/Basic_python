dfecf632f586c18bec68fb7280fbacaf0e41e1f5
Imesh <ikaushalya7172800@gmail.com>
2025-10-04 20:45:04 +0530

    clean code

--- number_guessing.py
+++ number_guessing.py
@@ -61,7 +61,7 @@ print("👋 Thanks for playing!")
 
  
        
-if guess == number_to_guess:
+    == number_to_guess:
     attempts += 1         # Counting the last attempt
     print(f"🎉 You guessed it in {attempts} attempts!")
 else:
