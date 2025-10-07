90152a6ae9f6b5fd6d4836ee96959826c4db7f34
Imesh <ikaushalya7172800@gmail.com>
2025-10-07 21:00:24 +0530

    clean code

--- number_guessing.py
+++ number_guessing.py
@@ -64,7 +64,7 @@ print("👋 Thanks for playing!")
                
   
  
-        your attempts. The number was {number_to_guess}.")
+          attempts. The number was {number_to_guess}.")
 
     
 print("-"*30)
